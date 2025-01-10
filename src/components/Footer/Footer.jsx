005a3b32b81f3ecb267cb9e4e76ee59ab6b27d35
Umbreen Arshad <umbreenvirk@gmail.com>
2025-01-10 10:50:03 -0800

    styling

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import './Footer.scss';
+
+function Footer () {
+    return (
+        <p className="footer">Powered by: <span className='footer__text'> ScotiaCrew</span></p>
+    )
+}
+
+export default Footer;
